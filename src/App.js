22207e284707c2bed76ce363d275de787ffd7ce0
ForageIt <katariaesha02@gmail.com>
2023-06-07 12:32:02 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SearchIcon from "./search.svg";
 import "./App.css";
 import axios from "axios";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
